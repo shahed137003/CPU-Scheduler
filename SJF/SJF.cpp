0513f20a5f438f577eb23967e0bfcba98c19f31d
MariamHany4 <mar.hany@hotmail.com>
2025-04-10 04:11:47 +0200

    Editing SJF

--- SJF/SJF.cpp
+++ SJF/SJF.cpp
@@ -1,50 +1,26 @@
 #include "SJF.h"
-void printGantt(vector<Process> p)
+void SJF(vector<Processes>& processes)
 {
-    cout << "Gantt Chart:\n";
-    for(int i = 0; i < p.size(); i++)
-    {
-        cout << p[i].name << " ";
-        for(int j = 0; j < p[i].burst_time; j++)
-            cout << "-";
-        cout << " ";
-    }
-    cout << endl;
-}
-void calc_avg_turn_around_time(vector<Process> processes)
-{
-    float total=0;
-    for(int i=0;i<processes.size();i++)
-        total+=processes[i].turn_around_time;
-    cout << "Average Turnaround Time: " << total / processes.size() << endl;
-}
-void calc_avg_waiting_time(vector<Process> processes)
-{
-    float total=0;
-    for(int i=0;i<processes.size();i++)
-        total+=processes[i].waiting_time;
-    cout << "Average Waiting Time: " << total / processes.size() << endl;
-}
-void SJF(vector<Process> processes)
-{
-    vector<Process> ordered_processes;
+    queue<char> ordered_processes_with_names;
+    queue<Processes> ordered_processes;
     float current_time = 0;
     float total_waiting_time=0;
+    queue<vector<float>>time_slots;
     float total_turn_around_time=0;
     while (!processes.empty())
     {
-        // we must sort the processes based on arrival time and burst time
-        sort(processes.begin(), processes.end(), [](const Process& a, const Process& b) {
-            if (a.arrival_time == b.arrival_time)
-                return a.burst_time < b.burst_time;
-            return a.arrival_time < b.arrival_time;
+        // we must sort processes based on burst time first, and arrival time second if burst times are equal
+        sort(processes.begin(), processes.end(), [](const Processes& a, const Processes& b) {
+            if (a.getBurst() == b.getBurst())
+                return a.getArrival() < b.getArrival();  // If burst times are the same, sort by arrival time
+            return a.getBurst() < b.getBurst();  // Otherwise, sort by burst time
         });
 
         // we need to find the process that has arrived and has the shortest burst time
-        Process selected_process;
+        Processes selected_process;
         bool process_found = false;
         for (int i = 0; i < processes.size(); i++) {
-            if (processes[i].arrival_time <= current_time) {
+            if (processes[i].getArrival() <= current_time) {
                 selected_process = processes[i];
                 processes.erase(processes.begin() + i);
                 process_found = true;
@@ -53,22 +29,18 @@ void SJF(vector<Process> processes)
         }
 
         if (process_found) {
-            ordered_processes.push_back(selected_process);
-            current_time += selected_process.burst_time;
-            selected_process.completion_time=current_time;
-            selected_process.turn_around_time=selected_process.completion_time-selected_process.arrival_time;
-            selected_process.waiting_time=selected_process.turn_around_time-selected_process.burst_time;
-            ordered_processes[ordered_processes.size() - 1] = selected_process;
-            total_turn_around_time+=selected_process.turn_around_time;
-            total_waiting_time+=selected_process.waiting_time;
+            ordered_processes_with_names.push(selected_process.getName());
+            time_slots.push({ current_time,current_time + selected_process.getBurst() });
+            current_time += selected_process.getBurst();
+
         }
         else
            current_time++; //If there is no process ready, increment the current time
     }
 
-    printGantt(ordered_processes);
-    calc_avg_turn_around_time(ordered_processes);
-    calc_avg_waiting_time(ordered_processes);
+    printGantt(ordered_processes_with_names,time_slots,true);
+    calcAvg_turn_time(ordered_processes);
+    calcAvg_wait_time(ordered_processes);
 }
 
 int main()
@@ -76,26 +48,31 @@ int main()
     string done = "Y";
     string type;
     int i = 0;
-    queue<Process> ready_queue;
+    float burst,arrival;
+    char name;
+    queue<Processes> ready_queue;
     cout << "Enter Scheduler Type: ";
     cin >> type;
     while (done == "Y" || done == "y")
     {
-        Process p;
+        Processes p;
         i++;
         cout << "Enter process " << i << " name: ";
-        cin >> p.name;
+        cin >> name;
+        p.setName(name);
         cout << "Enter process " << i << " arrival time: ";
-        cin >> p.arrival_time;
+        cin >> arrival;
+        p.setArrival(arrival);
         cout << "Enter process " << i << " burst time: ";
-        cin >> p.burst_time;
-        p.remaining_time = p.burst_time;
+        cin >> burst;
+        p.setBurst(burst);
+        p.setRemaining (burst);
         ready_queue.push(p);
         cout << "Do you want to add more processes? (Y/N) ";
         cin >> done;
     }
 
-    vector<Process> processes;
+    vector<Processes> processes;
     while (!ready_queue.empty()) {
         processes.push_back(ready_queue.front());
         ready_queue.pop();
--- SJF/SJF.h
+++ SJF/SJF.h
@@ -1,22 +1,3 @@
-#ifndef SJF_H
-#define SJF_H
-#include <iostream>
-#include <queue>
-#include <vector>
-#include <algorithm>
-using namespace std;
-struct Process {
-    string name;
-    float arrival_time;
-    float burst_time;
-    float remaining_time;
-    float completion_time;
-    float turn_around_time;
-    float waiting_time;
-};
+#include"..\Processes\Processes.h"
+void SJF(vector<Processes>& processes);
 
-void calc_avg_turn_around_time(vector<Process> processes);
-void calc_avg_waiting_time(vector<Process> processes);
-void SJF(vector<Process> processes);
-
-#endif
