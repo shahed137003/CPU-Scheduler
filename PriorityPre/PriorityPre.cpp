afaf4a20376e7383c75308347a5bdd86f051e03f
حبيبة مصطفى محمود قناوى <143437900+Habibakenawy@users.noreply.github.com>
2025-04-10 15:41:06 +0200

    finished prioritypre

--- PriorityPre/PriorityPre.cpp
+++ PriorityPre/PriorityPre.cpp
@@ -3,67 +3,67 @@
 
 void PriorityPre(vector<Processes>& processes)
 {
-
-    priority_queue<Processes,vector<Processes>,compareByPriority> pq;
-    float total_burst=0;
-    for(int i=0;i<processes.size();i++)
-    {
-        processes[i].setRemaining(processes[i].getBurst());
-        pq.push(processes[i]);
-        total_burst+=processes[i].getBurst();
+    float total_burst = 0;
+    for (auto& process : processes) {
+        process.setRemaining(process.getBurst());
+        total_burst += process.getBurst();
     }
 
-    for(float i=0;i<total_burst;i++){   
+    for (float i = 0; i < total_burst; i++) {
+        int selected = -1;
+        for (int j = 0; j < processes.size(); j++) {
+            if (processes[j].getArrival() <= i && processes[j].getRemaining() > 0) {
+                if (selected == -1 || processes[j].getPriority() < processes[selected].getPriority()) {
+                    selected = j;
+                }
+            }
+        }
 
-    for(auto& process:pq){ //const hna wala la
+        if (selected != -1) {
+            Processes& process = processes[selected];
+            process.setRemaining(process.getRemaining() - 1);
 
-    if(process.getArrival()<=i&&process.getRemaining()!=0){
-       process.setRemaining(process.getRemaining()-1);
-       if(process.getRemaining()==0&&process.getLasttime()!=1)
-       {
-        process.setLasttime(1);
-        float arr = process.getArrival();
-        float burst = process.getBurst();
-        float turnaround= i-arr;
-        float waiting=turnaround-burst;
-        process.setTurnaround(turnaround);
-        process.setWaiting(waiting);
-       }
-       break();
+            if (process.getRemaining() == 0 && process.getLasttime() == -1) {
+                float arr = process.getArrival();
+                float burst = process.getBurst();
+                float turnaround = i+1 - arr;
+                float waiting = turnaround - burst;
+                process.setLasttime(i+1);
+                process.setTurnaround(turnaround);
+                process.setWaiting(waiting);
+            }
+        }
     }
-   
+    queue<Processes> QueueOfProcesses;
+    for (const auto& p : processes) {
+        QueueOfProcesses.push(p);
     }
-
-    }
-
- 
-
-
-
-
-
+    cout << "Total Turnaround Time: " << calcTotal_turn_time(QueueOfProcesses) << "\n";
+    cout << "Average Turnaround Time: " << calcAvg_turn_time(QueueOfProcesses) << "\n\n";
+    cout << "Total Waiting Time: " << calcTotal_wait_time(QueueOfProcesses) << "\n";
+    cout << "Average Waiting Time: " << calcAvg_wait_time(QueueOfProcesses) << "\n";
 }
 
 
 
-
-   
+    
 
 
 int main() {
     vector<Processes> processList;
-    processList.push_back(Processes('A', 0, 5, 0));
-    processList.push_back(Processes('B', 1, 3, 1));
-    processList.push_back(Processes('C', 2, 8, 2));
-    processList.push_back(Processes('D', 3, 6, 3));
+    processList.push_back(Processes('A', 0, 5, 2)); //ashof lw same priority a3ml eh
+    processList.push_back(Processes('B', 1, 3, 0));
+    processList.push_back(Processes('C', 2, 8, 3));
+    processList.push_back(Processes('D', 3, 6, 1));
 
     PriorityPre(processList);
 
-    cout << "Name\tArrival\tBurst\tCompletion\tTurnaround\tWaiting\n";
+    cout << "Name\tArrival\tBurst\tPriority\tCompletion\tTurnaround\tWaiting\n";
     for (const auto& p : processList) {
         cout << p.getName() << "\t"
              << p.getArrival() << "\t"
              << p.getBurst() << "\t"
+             <<p.getPriority()<<"\t\t"
              << p.getLasttime() << "\t\t"
              << p.getTurnaround() << "\t\t"
              << p.getWaiting() << endl;
