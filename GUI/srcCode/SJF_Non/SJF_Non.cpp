c12df24f11f6c3be85e2e11d47f551a5c824c703
MariamHany4 <mar.hany@hotmail.com>
2025-04-18 07:52:05 +0200

    Fixing SJF_Non.cpp

--- GUI/srcCode/SJF_Non/SJF_Non.cpp
+++ GUI/srcCode/SJF_Non/SJF_Non.cpp
@@ -16,57 +16,76 @@ void SJF_Non::start() {
 
 void SJF_Non::processStep() {
     qDebug() << "SJF_Non::processStep called, processes size:" << processes.size();
-    queue<char> ordered_processes_with_names;
-    queue<Processes> ordered_processes;
+
     float current_time = 0;
-    queue<vector<float>>time_slots;
+    std::vector<Processes> remaining = processes;
+    std::queue<char> local_operate;
+    std::queue<std::vector<float>> local_time_slots;
+    std::queue<Processes> terminated;
 
+    while (!remaining.empty()) {
+        std::sort(remaining.begin(), remaining.end(), compareByBurst);
 
-    while (!processes.empty())
-    {
-        // we must sort processes based on burst time first, and arrival time second if burst times are equal
-        sort(processes.begin(), processes.end(),compareByBurst);
+        bool found = false;
+        for (int i = 0; i < remaining.size(); ++i) {
+            if (remaining[i].getArrival() <= current_time) {
+                Processes p = remaining[i];
+                remaining.erase(remaining.begin() + i);
 
-        // we need to find the process that has arrived and has the shortest burst time
-        Processes selected_process;
-        bool process_found = false;
-        int j = processes.size();
-        for (int i = 0; i < j; i++) {
-            if (processes[i].getArrival() <= current_time) {
-                selected_process = processes[i];
-                processes.erase(processes.begin() + i);
-                process_found = true;
+                local_operate.push(p.getName());
+                local_time_slots.push({ current_time, current_time + p.getBurst() });
+
+                float start_time = current_time;
+                float finish_time = current_time + p.getBurst();
+                p.setTurnaround(finish_time - p.getArrival());
+                p.setWaiting(p.getTurnaround() - p.getBurst());
+                p.setResponse(start_time - p.getArrival());
+
+                terminated.push(p);
+                current_time = finish_time;
+                found = true;
+
+                // Update Gantt chart live if enabled
+                if (live && gantt) {
+                    gantt->updateGanttChart(local_operate, local_time_slots, true);
+                    std::this_thread::sleep_for(std::chrono::milliseconds(1000)); // simulate delay
+                }
+
                 break;
             }
         }
 
-        if (process_found) {
-            ordered_processes_with_names.push(selected_process.getName());
-            ordered_processes.push(selected_process);
-            time_slots.push({ current_time,current_time + selected_process.getBurst() });
-            current_time += selected_process.getBurst();
-
+        if (!found) {
+            current_time++; // Idle CPU
         }
-        else
-            current_time++; //If there is no process ready, increment the current time
     }
+
+    // Store for final report and visualization
+    operate = local_operate;
+    time_slots = local_time_slots;
+    terminatedProcesses = terminated;
+
+    // Final update to Gantt chart (non-live or complete chart)
+    if (gantt) {
+        gantt->updateGanttChart(operate, time_slots, false);
+    }
+
     printResults();
 }
 
 QString SJF_Non::printResults() {
-    //processes = terminatedProcesses;
-    printGantt(operate, time_slots, live);
-
-    std::cout << "\n\n\n";
+    std::cout << "\n\n";
     std::cout << "Total Turnaround Time: " << calcTotal_turn_time(terminatedProcesses) << "\n";
     std::cout << "Average Turnaround Time: " << calcAvg_turn_time(terminatedProcesses) << "\n\n";
     std::cout << "Total Waiting Time: " << calcTotal_wait_time(terminatedProcesses) << "\n";
-    std::cout << "Average Waiting Time: " << calcAvg_wait_time(terminatedProcesses) << "\n";
-    QString results;
-    //results += QString("Total Turnaround Time: %1\n").arg(calcTotal_turn_time(terminatedProcesses));
-    results += QString("Average Turnaround Time: %1\n\n").arg(calcAvg_turn_time(terminatedProcesses));
-    //results += QString("Total Waiting Time: %1\n").arg(calcTotal_wait_time(terminatedProcesses));
-    results += QString("Average Waiting Time: %1\n").arg(calcAvg_wait_time(terminatedProcesses));
+    std::cout << "Average Waiting Time: " << calcAvg_wait_time(terminatedProcesses) << "\n\n";
+    std::cout << "Total Response Time: " << calcTotal_response_time(terminatedProcesses) << "\n";
+    std::cout << "Average Response Time: " << calcAvg_response_time(terminatedProcesses) << "\n";
 
+    QString results;
+    results += QString("Average Turnaround Time: %1\n").arg(calcAvg_turn_time(terminatedProcesses));
+    results += QString("Average Waiting Time: %1\n").arg(calcAvg_wait_time(terminatedProcesses));
+    results += QString("Average Response Time: %1\n").arg(calcAvg_response_time(terminatedProcesses));
     return results;
 }
+
